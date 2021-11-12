89bd2f88a8611be9d56396c3c322be64044ff9d8
Anullos <anullos988@gmail.com>
2021-11-12 16:52:08 +0100

    [FIREBASE]: Get data from firestore

--- lib/features/product/data/datasource/product_repository_implements.dart
+++ lib/features/product/data/datasource/product_repository_implements.dart
@@ -1,11 +1,21 @@
-import 'package:smartdream/features/product/data/products_data.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:smartdream/features/product/domain/models/product_model.dart';
 import 'package:smartdream/features/product/domain/repository/product_repository_interface.dart';
 
 class ProductRepositoryImplements extends ProductRepositoryInterface {
   @override
   Future<List<ProductModel>?> getAllProducts() async {
-    final products = productsData;
-    return products;
+    try {
+      final _firestore = FirebaseFirestore.instance;
+      QuerySnapshot _querySnapshot =
+          await _firestore.collection('products').get();
+      final allData = _querySnapshot.docs.map((doc) => doc.data()).toList();
+      print(allData.first.runtimeType);
+      List<ProductModel> products = List<ProductModel>.from(allData.map(
+          (x) => ProductModel.fromMap(x as Map<String, dynamic>))).toList();
+      return products;
+    } catch (e) {
+      return null;
+    }
   }
 }
