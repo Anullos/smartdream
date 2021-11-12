7d3ab8657da9960206d73aaecdca23bb0971dc92
Anullos <anullos988@gmail.com>
2021-11-12 18:47:04 +0100

    [FIX]: delete print

--- lib/features/product/data/datasource/product_repository_implements.dart
+++ lib/features/product/data/datasource/product_repository_implements.dart
@@ -10,7 +10,6 @@ class ProductRepositoryImplements extends ProductRepositoryInterface {
       QuerySnapshot _querySnapshot =
           await _firestore.collection('products').get();
       final allData = _querySnapshot.docs.map((doc) => doc.data()).toList();
-      print(allData.first.runtimeType);
       List<ProductModel> products = List<ProductModel>.from(allData.map(
           (x) => ProductModel.fromMap(x as Map<String, dynamic>))).toList();
       return products;
